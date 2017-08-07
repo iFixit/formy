539baba8cb9bf63e9f5202981e1cf05c0f5e1db6
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-07 15:35:23 -0700

    use es5 constructor

--- src/example/ExampleBasicForm.js
+++ src/example/ExampleBasicForm.js
@@ -2,31 +2,34 @@ import React from 'react';
 import Form from '../Formy/Form';
 
 class ExampleBasicForm extends React.Component {
-   state = {
-      form: {
-         name: 'signupForm',
-         onSubmit: Form.onSubmitFactory(data => console.log(data)),
-         fields: Form.fields({
-            onChange: Form.onChangeFactory(form => this.setState({ form })),
-         }, {
-            name: {
-               type: 'text',
-               label: 'Name',
-            },
-            email: {
-               type: 'email',
-               label: 'Email',
-            },
-            password: {
-               type: 'password',
-               label: 'Password'
-            },
-            newsletterSignup: {
-               type: 'checkbox',
-               label: 'Signup for our newsletter?',
-            },
-         }),
-      },
+   constructor(props) {
+      super(props);
+      this.state = {
+         form: {
+            name: 'signupForm',
+            onSubmit: Form.onSubmitFactory(data => console.log(data)),
+            fields: Form.fields({
+               onChange: Form.onChangeFactory(form => this.setState({ form })),
+            }, {
+               name: {
+                  type: 'text',
+                  label: 'Name',
+               },
+               email: {
+                  type: 'email',
+                  label: 'Email',
+               },
+               password: {
+                  type: 'password',
+                  label: 'Password'
+               },
+               newsletterSignup: {
+                  type: 'checkbox',
+                  label: 'Signup for our newsletter?',
+               },
+            }),
+         },
+      }
    }
 
    submitForm(data) {

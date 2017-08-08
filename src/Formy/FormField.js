9904ea056e8537d46c8d3a3ec998bf079015765f
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-08 14:27:38 -0700

    remove validity from formy

--- src/Formy/FormField.js
+++ src/Formy/FormField.js
@@ -3,37 +3,9 @@ import FormDefaultComponentLibrary from './FormDefaultComponentLibrary';
 
 let FormField = {};
 
-FormField.Component = class FormFieldComponent extends React.Component {
-   constructor(props) {
-      super(props);
-      this.setFieldRef = this.setFieldRef.bind(this);
-      this.fieldRef = {
-         validationMessage: '',
-         validity: { valid: true },
-      };
-   }
-
-   setFieldRef(fieldRef) {
-      this.fieldRef = fieldRef;
-   }
-
-   // Immediately re render when the validity state can be accessed
-   componentDidMount() {
-      this.forceUpdate();
-   }
-
-   render() {
-      const Component = this.props.componentLibrary[this.props.type];
-
-      return (
-         <Component
-            {...this.props}
-            valid={this.fieldRef.validity.valid}
-            validationMessage={this.fieldRef.validationMessage}
-            fieldRef={this.setFieldRef}
-         />
-      );
-   }
+FormField.Component = props => {
+   const Component = props.componentLibrary[props.type];
+   return <Component {...props} />;
 }
 
 FormField.Component.defaultProps = {

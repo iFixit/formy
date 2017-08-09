5bbbb5acfd8d0e17aceb9b1c723059da1413f220
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-09 09:01:34 -0700

    add proptypes to FormFieldComponent

--- src/Formy/FormField.js
+++ src/Formy/FormField.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import FormDefaultComponentLibrary from './FormDefaultComponentLibrary';
 
 let FormField = {};
@@ -18,6 +19,23 @@ FormField.Component.defaultProps = {
    checked: false,
    radios: [],
    componentLibrary: FormDefaultComponentLibrary,
+};
+
+FormField.Component.propTypes = {
+   type: PropTypes.string.isRequired,
+   value: PropTypes.string.isRequired,
+   disabled: PropTypes.bool.isRequired,
+   checked: PropTypes.bool.isRequired,
+   radios: PropTypes.array.isRequired,
+   componentLibrary: PropTypes.object.isRequired,
+   required: (props, propName, componentName) => (
+      typeof props[propName] === 'boolean' &&
+      props[propName] &&
+      props.type === 'radiogroup' ? new Error(
+         'Invalid prop `' + propName + '` supplied to' +
+         ' `' + componentName + '`. Validation failed.'
+      ) : null
+   ),
 };
 
 export default FormField;
--- src/example/ExampleBasicForm.js
+++ src/example/ExampleBasicForm.js
@@ -4,32 +4,33 @@ import Form from '../Formy/Form';
 class ExampleBasicForm extends React.Component {
    constructor(props) {
       super(props);
-      this.state = {
-         form: {
-            name: 'signupForm',
-            onSubmit: Form.onSubmitFactory(data => console.log(data)),
-            fields: Form.fields({
-               onChange: Form.onChangeFactory(form => this.setState({ form })),
-            }, {
-               name: {
-                  type: 'text',
-                  label: 'Name',
-               },
-               email: {
-                  type: 'email',
-                  label: 'Email',
-               },
-               password: {
-                  type: 'password',
-                  label: 'Password'
-               },
-               newsletterSignup: {
-                  type: 'checkbox',
-                  label: 'Signup for our newsletter?',
-               },
-            }),
-         },
+
+      const form = {
+         name: 'signupForm',
+         onSubmit: Form.onSubmitFactory(data => console.log(data)),
+         fields: Form.fields({
+            onChange: Form.onChangeFactory(form => this.setState({ form })),
+         }, {
+            name: {
+               type: 'text',
+               label: 'Name',
+            },
+            email: {
+               type: 'email',
+               label: 'Email',
+            },
+            password: {
+               type: 'password',
+               label: 'Password',
+            },
+            newsletterSignup: {
+               type: 'checkbox',
+               label: 'Signup for our newsletter?',
+            },
+         }),
       };
+
+      this.state = { form };
    }
 
    render() {

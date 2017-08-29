cac86f28e273813450cddc31b44e8ec25364f643
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-28 17:21:24 -0700

    add example cases for constraint validation

--- src/example/App.js
+++ src/example/App.js
@@ -3,6 +3,12 @@ import ExampleBasicForm from './ExampleBasicForm';
 import ExampleFormProperties from './ExampleFormProperties';
 import ExampleComputedForm from './ExampleComputedForm';
 import ExampleCustomComponentLibrary from './ExampleCustomComponentLibrary';
+import ExampleValidityForm from './ExampleValidityForm';
+import ExampleMaxLength from './ExampleMaxLength';
+import ExampleMinLength from './ExampleMinLength';
+import ExamplePattern from './ExamplePattern';
+import ExampleMin from './ExampleMin';
+import ExampleMax from './ExampleMax';
 
 class App extends React.Component {
    render() {
@@ -26,6 +32,48 @@ class App extends React.Component {
             <section>
                <h1>Custom componentLibrary</h1>
                <ExampleCustomComponentLibrary/>
+            </section>
+
+            <section>
+               <h1>Form with validity checking</h1>
+               <p>Expected UX:</p>
+               <ul>
+                  <li>Clicking submit displays a field's invalid state if it's invalid</li>
+                  <li>Focusing a field display's its validation message</li>
+                  <li>Invalid state is removed when the value has been properly updated</li>
+                  <li>The form isn't submitted (check the console.logs) if there's an invalid field</li>
+               </ul>
+               <ExampleValidityForm/>
+            </section>
+
+            <section>
+               <h1>Max Number Value: 3</h1>
+               <a href="https://www.w3.org/TR/html5/forms.html#attr-input-max">Spec</a>
+               <ExampleMax/>
+            </section>
+
+            <section>
+               <h1>Max Length: 3</h1>
+               <a href="https://www.w3.org/TR/html5/forms.html#attr-fe-maxlength">Spec</a>
+               <ExampleMaxLength/>
+            </section>
+
+            <section>
+               <h1>Min Number Value: 3</h1>
+               <a href="https://www.w3.org/TR/html5/forms.html#attr-input-min">Spec</a>
+               <ExampleMin/>
+            </section>
+
+            <section>
+               <h1>Min Length: 3</h1>
+               <a href="https://www.w3.org/TR/html5/forms.html#attr-fe-minlength">Spec</a>
+               <ExampleMinLength/>
+            </section>
+
+            <section>
+               <h1>Pattern: abcdefg</h1>
+               <a href="https://www.w3.org/TR/html5/forms.html#attr-input-pattern">Spec</a>
+               <ExamplePattern/>
             </section>
          </div>
       );
---
+++ src/example/ExampleMaxLength.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Form from '../Formy/Form';
+
+class ExampleMaxLength extends React.Component {
+   constructor(props) {
+      super(props);
+
+      const form = {
+         name: 'signupForm',
+         onSubmit: Form.onSubmitFactory(data => console.log(data)),
+         fields: Form.fields({
+            onChange: Form.onChangeFactory(form => this.setState({ form })),
+         }, {
+            name: {
+               maxLength: 3,
+            },
+         }),
+      };
+
+      this.state = { form };
+   }
+
+   render() {
+      const form = Form.getProps(this.state.form);
+
+      return(
+         <Form.Component {...form}>
+            <Form.Field {...form.fields.name}/>
+            <button type="submit">Submit</button>
+         </Form.Component>
+      );
+   }
+}
+
+export default ExampleMaxLength;

fe169577a07e0dbf49491eca82ccd670443d02a3
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-28 17:15:23 -0700

    add constraint validation types

--- src/Formy/FormField.js
+++ src/Formy/FormField.js
@@ -12,10 +12,8 @@ FormField.defaultProps = {
    value: '',
    label: '',
    placeholder: '',
-   description: '',
    autocomplete: 'off',
    disabled: false,
-   required: false,
    checked: false,
    radios: [],
    componentLibrary: FormDefaultComponentLibrary,
@@ -25,29 +23,83 @@ FormField.propTypes = {
    autocomplete: PropTypes.string.isRequired,
    checked: PropTypes.bool.isRequired,
    componentLibrary: PropTypes.object.isRequired,
-   description: PropTypes.string.isRequired,
    disabled: PropTypes.bool.isRequired,
    label: PropTypes.string.isRequired,
    name: PropTypes.string.isRequired,
    onChange: PropTypes.func.isRequired,
-   placeholder: PropTypes.string.isRequired,
+   placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
    radios: PropTypes.array.isRequired,
-   required: (props, propName, componentName) => (
-      typeof props[propName] === 'boolean' &&
-      props[propName] &&
-      props.type === 'radiogroup' ?
-      new Error(
+   type: PropTypes.string.isRequired,
+   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+
+   // Constraint validation: https://www.w3.org/TR/html5/forms.html#candidate-for-constraint-validation
+
+   // https://www.w3.org/TR/html5/forms.html#attr-input-max
+   max: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+   ]),
+
+   // https://www.w3.org/TR/html5/forms.html#attr-fe-maxlength
+   maxLength: (props, propName, componentName) => (
+      props[propName] === undefined ||
+      (
+         Number.isInteger(props[propName]) &&
+         (props[propName] >= 0)
+      ) ?
+      null : new Error(
          'Invalid prop `' + propName + '` supplied to' +
          ' `' + componentName + '`. Validation failed.'
-      ) : null
+      )
    ),
-   type: PropTypes.string.isRequired,
-   value: PropTypes.string.isRequired,
+
+   // https://www.w3.org/TR/html5/forms.html#attr-input-min
+   min: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+   ]),
+
+   // https://www.w3.org/TR/html5/forms.html#attr-fe-minlength
+   minLength: (props, propName, componentName) => (
+      props[propName] === undefined ||
+      (
+         Number.isInteger(props[propName]) &&
+         (props[propName] >= 0)
+      ) ?
+      null : new Error(
+         'Invalid prop `' + propName + '` supplied to' +
+         ' `' + componentName + '`. Validation failed.'
+      )
+   ),
+
+   // https://www.w3.org/TR/html5/forms.html#attr-input-pattern
    pattern: PropTypes.string,
-   min: PropTypes.string,
-   max: PropTypes.string,
-   step: PropTypes.string,
-   maxLength: PropTypes.string,
+
+   // https://www.w3.org/TR/html5/forms.html#attr-input-required
+   required: (props, propName, componentName) => (
+      props[propName] === undefined ||
+      (
+         typeof props[propName] === 'boolean' &&
+         props.type !== 'radiogroup'
+      ) ?
+      null : new Error(
+         'Invalid prop `' + propName + '` supplied to' +
+         ' `' + componentName + '`. Validation failed.'
+      )
+   ),
+
+   // https://www.w3.org/TR/html5/forms.html#attr-input-step
+   step: (props, propName, componentName) => (
+      props[propName] === undefined ||
+      (
+         !Number.isNaN(props[propName]) ||
+         (props[propName] === 'any')
+      ) ?
+      null : new Error(
+         'Invalid prop `' + propName + '` supplied to' +
+         ' `' + componentName + '`. Validation failed.'
+      )
+   ),
 };
 
 export default FormField;

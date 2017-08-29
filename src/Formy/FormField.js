72f0a512809c9dd54f42571ebad36e92e5381e84
Tyler Deitz <tylerdeitz@gmail.com>
2017-08-29 13:49:21 -0700

    generate custom validity simpler

--- src/Formy/FormField.js
+++ src/Formy/FormField.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import FormFieldPropTypes from './FormFieldPropTypes';
 import FormDefaultComponentLibrary from './FormDefaultComponentLibrary';
 
 const FormField = ({componentLibrary, ...props}) => {
@@ -14,83 +14,6 @@ FormField.defaultProps = {
    value: '',
 };
 
-FormField.propTypes = {
-   // Required
-   checked: PropTypes.bool.isRequired,
-   componentLibrary: PropTypes.object.isRequired,
-   name: PropTypes.string.isRequired,
-   type: PropTypes.string.isRequired,
-   value: PropTypes.string.isRequired,
-
-   // Not required
-   autocomplete: PropTypes.string,
-   disabled: PropTypes.bool,
-   label: PropTypes.string,
-   onChange: PropTypes.func,
-   placeholder: PropTypes.string,
-   radios: PropTypes.array,
-
-   // Constraint validation
-   // https://www.w3.org/TR/html5/forms.html#attr-input-max
-   max: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-   ]),
-   // https://www.w3.org/TR/html5/forms.html#attr-fe-maxlength
-   maxLength: (props, propName, componentName) => (
-      props[propName] === undefined ||
-      (
-         Number.isInteger(props[propName]) &&
-         (props[propName] >= 0)
-      ) ?
-      null : new Error(
-         'Invalid prop `' + propName + '` supplied to' +
-         ' `' + componentName + '`. Validation failed.'
-      )
-   ),
-   // https://www.w3.org/TR/html5/forms.html#attr-input-min
-   min: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-   ]),
-   // https://www.w3.org/TR/html5/forms.html#attr-fe-minlength
-   minLength: (props, propName, componentName) => (
-      props[propName] === undefined ||
-      (
-         Number.isInteger(props[propName]) &&
-         (props[propName] >= 0)
-      ) ?
-      null : new Error(
-         'Invalid prop `' + propName + '` supplied to' +
-         ' `' + componentName + '`. Validation failed.'
-      )
-   ),
-   // https://www.w3.org/TR/html5/forms.html#attr-input-pattern
-   pattern: PropTypes.string,
-   // https://www.w3.org/TR/html5/forms.html#attr-input-required
-   required: (props, propName, componentName) => (
-      props[propName] === undefined ||
-      (
-         typeof props[propName] === 'boolean' &&
-         props.type !== 'radiogroup'
-      ) ?
-      null : new Error(
-         'Invalid prop `' + propName + '` supplied to' +
-         ' `' + componentName + '`. Validation failed.'
-      )
-   ),
-   // https://www.w3.org/TR/html5/forms.html#attr-input-step
-   step: (props, propName, componentName) => (
-      props[propName] === undefined ||
-      (
-         !Number.isNaN(props[propName]) ||
-         (props[propName] === 'any')
-      ) ?
-      null : new Error(
-         'Invalid prop `' + propName + '` supplied to' +
-         ' `' + componentName + '`. Validation failed.'
-      )
-   ),
-};
+FormField.propTypes = FormFieldPropTypes;
 
 export default FormField;
---
+++ src/Formy/FormFieldPropTypes.js
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+
+const customValidity = (constraint, required = false) => (
+   props,
+   propName,
+   componentName
+) => {
+   return (
+   (!required && typeof props[propName] === 'undefined') ||
+   (
+      typeof props[propName] !== 'undefined' &&
+      constraint(props, propName, componentName)
+   ) ?
+   null : new Error(
+      `Invalid prop \`${propName}\` supplied to \`${componentName}\`. Validation failed.`
+   )
+)};
+
+const FormFieldPropTypes = {
+   // Required
+   checked: PropTypes.bool.isRequired,
+   componentLibrary: PropTypes.object.isRequired,
+   name: PropTypes.string.isRequired,
+   type: PropTypes.string.isRequired,
+   value: PropTypes.string.isRequired,
+
+   // Not required
+   autocomplete: PropTypes.string,
+   disabled: PropTypes.bool,
+   label: PropTypes.string,
+   onChange: PropTypes.func,
+   placeholder: PropTypes.string,
+   radios: PropTypes.array,
+
+   // Constraint validation
+   // https://www.w3.org/TR/html5/forms.html#attr-input-max
+   max: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+   ]),
+   // https://www.w3.org/TR/html5/forms.html#attr-fe-maxlength
+   maxLength: customValidity((props, propName, componentName) => (
+      Number.isInteger(props[propName]) &&
+      (props[propName] >= 0)
+   )),
+   // https://www.w3.org/TR/html5/forms.html#attr-input-min
+   min: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+   ]),
+   // https://www.w3.org/TR/html5/forms.html#attr-fe-minlength
+   minLength: customValidity((props, propName, componentName) => (
+      Number.isInteger(props[propName]) &&
+      (props[propName] >= 0)
+   )),
+   // https://www.w3.org/TR/html5/forms.html#attr-input-pattern
+   pattern: PropTypes.string,
+   // https://www.w3.org/TR/html5/forms.html#attr-input-required
+   required: customValidity((props, propName, componentName) => (
+      typeof props[propName] === 'boolean' &&
+      props.type !== 'radiogroup'
+   )),
+   // https://www.w3.org/TR/html5/forms.html#attr-input-step
+   step: customValidity((props, propName, componentName) => (
+      !Number.isNaN(props[propName]) ||
+      (props[propName] === 'any')
+   )),
+};
+
+export default FormFieldPropTypes;
